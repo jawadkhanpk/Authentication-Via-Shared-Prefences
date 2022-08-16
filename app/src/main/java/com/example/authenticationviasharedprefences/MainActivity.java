8e2f4324d6012334f1c277cfd30b5b59d1eb71c1
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-08-16 15:54:08 +0500

    activities creation

--- app/src/main/java/com/example/authenticationviasharedprefences/MainActivity.java
+++ app/src/main/java/com/example/authenticationviasharedprefences/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.authenticationviasharedprefences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Splash Screen Timer to Navigate from MainActivity to HomeActivity
+        Thread wait=new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
+                }
+            }
+        };wait.start();
+
     }
 }
